0a00acb6ba194eb64ab71d984205226998ca61fd
Jeremy Woertink <jeremywoertink@gmail.com>
2022-07-11 07:55:46 -0700

    Testing that arrays from form fields works

--- src/components/neighborhoods/form_fields.cr
+++ src/components/neighborhoods/form_fields.cr
@@ -11,5 +11,24 @@ class Neighborhoods::FormFields < BaseComponent
       label_for(operation.master_plan)
       checkbox(operation.master_plan)
     end
+    div do
+      amenities_list.each do |amenity|
+        label(amenity, for: "neighborhood_amenities_#{amenity}")
+        input type: "checkbox",
+          id: "neighborhood_amenities_#{amenity}",
+          name: "neighborhood:amenities[]",
+          value: amenity,
+          attrs: operation.amenities.value.try(&.includes?(amenity)) ? [:checked] : [] of Symbol
+      end
+    end
+  end
+
+  private def amenities_list
+    [
+      "Pool House",
+      "Security Gate",
+      "Golf Course",
+      "Park"
+    ]
   end
 end
--- src/operations/save_neighborhood.cr
+++ src/operations/save_neighborhood.cr
@@ -1,3 +1,3 @@
 class SaveNeighborhood < Neighborhood::SaveOperation
-  permit_columns name, hoa_fee, lid_fee, sid_fee, master_plan
+  permit_columns name, hoa_fee, lid_fee, sid_fee, master_plan, amenities
 end
